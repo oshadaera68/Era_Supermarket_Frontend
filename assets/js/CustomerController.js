87cf25545dc313c17b27b16a784e61af2c606400
oshadaera68 <mindartlk010@gmail.com>
2022-05-12 08:46:53 +0530

    updated index.html file and style.css

---
+++ assets/js/CustomerController.js
@@ -0,0 +1,10 @@
+/**
+ *@Owner - Oshada Eranga
+ *@Version - v0.1.0
+ **/
+
+$(".customerHomeNavi").click(function () {
+    /*$("#carouselExampleIndicators").css('display', 'block');*/
+    $("#customerPage").css('display', 'none');
+});
+
